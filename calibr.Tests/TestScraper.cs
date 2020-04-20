0fd46bcc3e2fc81a7736670533ada00a0fa9a6c1
Aron.Arany-Takacs <Aron.Arany-Takacs@CSC-SCC.gc.ca>
2020-04-20 10:37:45 -0400

    First Push

---
+++ calibr.Tests/TestScraper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scraper;
+
+namespace calibr.Tests
+{
+    [TestClass]
+    class TestScraper
+    {
+        [TestMethod]
+        public void TestReplaceMethod()
+        {
+            string baseStr = "https://[DOMAIN]/ads/";
+            string substitutionStr = "www.gunsexchange.ca";
+            string expectedStr = "https://www.gunsexchange.ca/ads/";
+
+            UriLoader ldr = new UriLoader();
+            ldr.ReplaceField( ref baseStr, "DOMAIN", substitutionStr);
+            Assert.AreEqual(expectedStr, baseStr);
+        }
+    }
+}
