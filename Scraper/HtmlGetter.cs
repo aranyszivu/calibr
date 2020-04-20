c8641e2179c8934baa1e9295a2b4c7567b09553d
Aron.Arany-Takacs <Aron.Arany-Takacs@CSC-SCC.gc.ca>
2020-04-20 14:40:33 -0400

    Rebuilding Scraper

---
+++ Scraper/HtmlGetter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BL = BusinessLayer;
+
+namespace Scraper
+{
+    class HtmlGetter
+    {
+        public List<HtmlDocument> GetHtmlDocuments(BL.SearchParameters parameters)
+        {
+            return null;
+        }
+        public HtmlDocument GetHtmlDocument(BL.SearchParameters parameters) 
+        { 
+            return GetHtmlResponse(BuildUrl(parameters));  
+        }
+
+        public string BuildUrl(BL.SearchParameters parameters)
+        {
+            return UrlBuilder.BuildUrl(parameters);
+        }
+
+        public HtmlDocument GetHtmlResponse(string strURL)
+        {
+            return null;
+        }
+    }
+}
---
+++ Scraper/UrlBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scraper
+{
+    static class UrlBuilder
+    {
+        public static string BuildUrl(BusinessLayer.SearchParameters parameters)
+        {
+            return null;
+        }
+    }
+}
