084b346852b19e1d1ee9209a9eb1b798b232f470
Aron.Arany-Takacs <Aron.Arany-Takacs@CSC-SCC.gc.ca>
2020-04-20 13:32:04 -0400

    Updated project formats

---
+++ HtmlScraper/HtmlGetter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Text;
+
+namespace Scraper
+{
+    class HtmlGetter
+    {
+        public List<HtmlDocument> GetHtmlDocuments()
+        {
+            return null;
+        }
+
+        public HtmlDocument GetHtmlDocument()
+        {
+            return null;
+        }
+
+        public string BuildUrl()
+        {
+            return null;
+        }
+
+        public HtmlDocument GetHtmlResponse()
+        {
+            return null;
+        }
+    }
+}
---
+++ Scraper/HtmlGetter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Text;
+
+namespace Scraper
+{
+    class HtmlGetter
+    {
+        public List<HtmlDocument> GetHtmlDocuments()
+        {
+
+        }
+    }
+}
