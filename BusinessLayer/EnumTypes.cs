167d5b592e1fa644441a4afd961a730e8b770c4d
Aron.Arany-Takacs <Aron.Arany-Takacs@CSC-SCC.gc.ca>
2020-04-20 13:53:00 -0400

    Added BusinessLayer

---
+++ BusinessLayer/EnumTypes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public static class EnumTypes
+    {
+        public enum Websites
+        {
+            GunPost,
+            FirearmsCanada,
+            TownPost
+        }
+        public enum Regions
+        {
+            Alberta,
+            BritishColumbia,
+            Manitoba,
+            NewBrunswick,
+            NewfoundlandAndLabrador,
+            NovaScotia,
+            Nunavut,
+            NorthwestTerritories,
+            Ontario,
+            PrinceEdwardIsland,
+            Quebec,
+            Saskatchewan,
+            Yukon
+        }
+        //Use cities included by target sites
+        public enum Cities
+        {
+            GTA,
+            NationalCapitalRegion
+        }
+    }
+}
---
+++ BusinessLayer/SearchParameters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class SearchParameters
+    {
+        public EnumTypes.Websites TargetSite;
+        public EnumTypes.Regions Region;
+        public EnumTypes.Cities City;
+        public string Keywords;
+    }
+}
